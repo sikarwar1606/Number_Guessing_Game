5bbf4c0d99149b6b3e5aa0e93d55f10a8ca95c3c
Anupam Singh Sikarwar <sikarwar1606@gmail.com>
2025-05-10 13:48:13 +0530

    Added emojis for user friendly

--- Guess_The_Number.py
+++ Guess_The_Number.py
@@ -3,7 +3,7 @@ win_count = 0 # Variable to keep track of wins
 total_count = 0 # Variable to keep track of total attempts
 random_number = 0 # Variable to store the random number
 
-print("Welcome to the Guess the Number game!")
+print("🎮Welcome to the Guess the Number game!")
 def check_guess():
     global win_count, total_count, random_number
     if win_count <=10:
@@ -14,20 +14,20 @@ def check_guess():
     try:
         user_input = int(input("Guess a number between 1 and 10: "))
     except ValueError:
-        print("Invalid input. Please enter a number.")
+        print("❌Invalid input. Please enter a number.")
         check_guess()
         return
     if user_input==random_number:
-        print("Congratulations! You guessed the number.")
+        print("🎉Congratulations! You guessed the number.")
         win_count += 1
         total_count += 1
-        print(f"Your score is {win_count}/{total_count}")
+        print(f"📊Your score is {win_count}/{total_count}")
         
         check_guess()
     else:
-        print(f"You guessed wrong. Correct number was {random_number}")
+        print(f"❌You guessed wrong. Correct number was {random_number}")
         total_count += 1
-        print(f"Your score is {win_count}/{total_count}") 
+        print(f"📊Your score is {win_count}/{total_count}") 
         
         check_guess()
 check_guess()
